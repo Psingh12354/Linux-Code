2fe3f91228bf63c26f638de549789d0d780a6616
PRIYANSHU SINGH <55645997+Psingh12354@users.noreply.github.com>
2020-09-10 15:24:17 +0530

    Update Calculator.sh

--- Calculator.sh
+++ Calculator.sh
@@ -1,3 +1,4 @@
+// pre defing input
 #!/bin/bash
 function add()
 {
@@ -21,6 +22,35 @@ function div()
 }
 a=40
 b=20
+add $a $b
+sub $a $b
+mul $a $b
+div $a $b
+// user given input
+function add()
+{
+    sum=$(($1 + $2))
+    echo "Sum = $sum"
+}
+function sub()
+{
+    minus=$(($1 - $2))
+    echo "Subtract = $minus"
+}
+function mul()
+{
+    multiply=$(($1 * $2))
+    echo "Multipy = $multiply"
+}
+function div()
+{
+    divide=$(($1 / $2))
+    echo "Divide = $divide"
+}
+echo "First Number : "
+read a
+echo "Second Number :  "
+read b
 add $a $b
 sub $a $b
 mul $a $b
